bc335303c2282ad6dc12a6a471571d36d6f51933
Georg Habeck <geha00@gmail.com>
2015-10-08 15:00:04 +0200

    Initial commit with basic file structure

---
+++ js/index.js
@@ -0,0 +1,22 @@
+var no = 1567
+var xmlhttp = new XMLHttpRequest();
+var url = "info.0.json";
+//document.getElementById("id01").innerHTML = url;
+
+xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+        var responseArray = JSON.parse(xmlhttp.responseText);
+        parseJSON(responseArray);
+    }
+}
+
+xmlhttp.open("GET", url);
+xmlhttp.send(null);
+
+function parseJSON(arr) {
+    document.getElementsByTagName("title")[0].innerHTML = arr["title"];
+    document.getElementsByTagName("h1")[0].innerHTML = arr["title"];
+    document.getElementsByTagName("img")[0].setAttribute("src", arr["img"]);
+    document.getElementById("alt").innerHTML = arr["alt"];
+    document.getElementById("num").innerHTML = arr["num"];
+}
