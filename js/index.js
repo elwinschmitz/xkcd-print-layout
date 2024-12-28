b73e39e80d5dc3688360abbb06d697e55e79409b
Elwin Schmitz <ik@elwin.nu>
2024-12-28 20:28:58 +0100

    refactor: Convert to use fetch instead of XHR

--- js/index.js
+++ js/index.js
@@ -1,20 +1,18 @@
-var xmlhttp = new XMLHttpRequest();
-var url = 'info.0.json';
 
-xmlhttp.onreadystatechange = function () {
-  if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-    var responseArray = JSON.parse(xmlhttp.responseText);
-    parseJSON(responseArray);
-  }
-};
+fetch('info.0.json')
+  .then((response) => response.json())
+  .then((data) => {
+    document.body.removeAttribute('hidden');
 
-xmlhttp.open('GET', url);
-xmlhttp.send(null);
+    document.title = data.title;
 
-function parseJSON(arr) {
-  document.getElementsByTagName('title')[0].innerHTML = arr['title'];
-  document.getElementsByTagName('h1')[0].innerHTML = arr['title'];
-  document.getElementsByTagName('img')[0].setAttribute('src', arr['img']);
-  document.getElementById('alt').innerHTML = arr['alt'];
-  document.getElementById('num').innerHTML = arr['num'];
-}
+    document.getElementById('title').innerHTML = data.title;
+    document.getElementById('num').innerHTML = data.num;
+
+    document.getElementById('img').src = data.img;
+    document.getElementById('img').alt = data.alt;
+    document.getElementById('img').title = data.alt;
+
+    document.getElementById('alt').innerHTML = data.alt;
+  })
+  .catch((error) => console.error('Error fetching the comic:', error));
