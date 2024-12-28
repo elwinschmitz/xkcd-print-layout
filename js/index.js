d0593fb7e1fa65ff237be8ecd38498bc55b26256
Elwin Schmitz <ik@elwin.nu>
2024-12-28 20:23:07 +0100

    chore: Format whitespace consistently

--- js/index.js
+++ js/index.js
@@ -1,22 +1,21 @@
-var no = 1567
+var no = 1567;
 var xmlhttp = new XMLHttpRequest();
-var url = "info.0.json";
-//document.getElementById("id01").innerHTML = url;
+var url = 'info.0.json';
 
-xmlhttp.onreadystatechange = function() {
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-        var responseArray = JSON.parse(xmlhttp.responseText);
-        parseJSON(responseArray);
-    }
-}
+xmlhttp.onreadystatechange = function () {
+  if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+    var responseArray = JSON.parse(xmlhttp.responseText);
+    parseJSON(responseArray);
+  }
+};
 
-xmlhttp.open("GET", url);
+xmlhttp.open('GET', url);
 xmlhttp.send(null);
 
 function parseJSON(arr) {
-    document.getElementsByTagName("title")[0].innerHTML = arr["title"];
-    document.getElementsByTagName("h1")[0].innerHTML = arr["title"];
-    document.getElementsByTagName("img")[0].setAttribute("src", arr["img"]);
-    document.getElementById("alt").innerHTML = arr["alt"];
-    document.getElementById("num").innerHTML = arr["num"];
+  document.getElementsByTagName('title')[0].innerHTML = arr['title'];
+  document.getElementsByTagName('h1')[0].innerHTML = arr['title'];
+  document.getElementsByTagName('img')[0].setAttribute('src', arr['img']);
+  document.getElementById('alt').innerHTML = arr['alt'];
+  document.getElementById('num').innerHTML = arr['num'];
 }
